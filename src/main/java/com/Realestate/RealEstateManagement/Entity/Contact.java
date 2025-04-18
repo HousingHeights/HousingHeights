0989bf028a2b67bb86020c603e21e7575c3fdc85
nitheeshniranjan <nitheeshm678@gmail.com>
2025-04-18 18:03:28 +0530

    Initial commit of backend files

---
+++ src/main/java/com/Realestate/RealEstateManagement/Entity/Contact.java
@@ -0,0 +1,64 @@
+package com.Realestate.RealEstateManagement.Entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "contact")
+public class Contact {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "full_name")
+    private String fullName;
+    private String email;
+    private String phone;
+    private String message;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", fullName=" + fullName + ", email=" + email + ", phone=" + phone + ", message="
+				+ message + "]";
+	}
+    
+    
+}
